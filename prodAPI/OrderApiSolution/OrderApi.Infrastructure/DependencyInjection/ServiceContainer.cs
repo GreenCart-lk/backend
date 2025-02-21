91a33a773b13af3dd64b662dbe7a7cab0af78cf3
DilanFdo <priyankarad276@gmail.com>
2025-02-21 21:28:47 +0530

    service container

---
+++ prodAPI/OrderApiSolution/OrderApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,37 @@
+﻿using Lib.Dependencyinjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderApi.Application.Interfaces;
+using OrderApi.Infrastructure.Data;
+using OrderApi.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApi.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration config)
+        {
+            //add database Connectivity
+            //add authentication scheme
+            SharedServiceContainer.AddSharedServices<OrderDbContext>(services, config, config["MySerilogLFileName"]!);
+
+            //create dependency injection
+            services.AddScoped<IOrder, OrderRepository>();
+            return services;
+        }
+        public static IApplicationBuilder UserInfrastructurePolicy(this IApplicationBuilder app)
+        {
+            //register middleware such as;
+            //global exception -> handle external errors
+            ////ListenToApiGatewaye only -> block all outsiders calls
+            SharedServiceContainer.UseSharedPolicies(app);
+            return app;
+        }
+    }
+}
