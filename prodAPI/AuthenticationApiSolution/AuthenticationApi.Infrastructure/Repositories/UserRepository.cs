9298c276e7e990a743db2571bc7a5157321200a3
DilanFdo <priyankarad276@gmail.com>
2025-02-22 21:42:51 +0530

    login

--- prodAPI/AuthenticationApiSolution/AuthenticationApi.Infrastructure/Repositories/UserRepository.cs
+++ prodAPI/AuthenticationApiSolution/AuthenticationApi.Infrastructure/Repositories/UserRepository.cs
@@ -5,6 +5,10 @@ using AuthenticationApi.Infrastructure.Data;
 using Lib.Responses;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 
 namespace AuthenticationApi.Infrastructure.Repositories
@@ -27,9 +31,41 @@ namespace AuthenticationApi.Infrastructure.Repositories
                 user.Role!) : null!;
         }
 
-        public Task<Response> Login(LoginDTO loginDTO)
+        public async Task<Response> Login(LoginDTO loginDTO)
         {
-            throw new NotImplementedException();
+            var getUser = await GetUserByEmail(loginDTO.Email);
+            if (getUser is null)
+                return new Response(false, "Invalid credentials");
+
+            bool verifyPassword = BCrypt.Net.BCrypt.Verify(loginDTO.Password, getUser.Password);
+            if (!verifyPassword) return new Response(false, "Invalid credentials");
+
+            string token = GenerateToken(getUser);
+            return new Response(true, token);
+        }
+
+        private string GenerateToken(AppUser user)
+        {
+            var key = Encoding.UTF8.GetBytes(config.GetSection("Authentication:Key").Value);
+            var securitykey = new SymmetricSecurityKey(key);
+            var credentials = new SigningCredentials(securitykey, SecurityAlgorithms.HmacSha256);
+            var claims = new List<Claim>
+            {
+                new(ClaimTypes.Name, user.Name!),
+                new(ClaimTypes.Email, user.Email!),
+                
+            };
+            if (!string.IsNullOrEmpty(user.Role) || !Equals("string", user.Role))
+                claims.Add(new(ClaimTypes.Role, user.Role!));
+
+            var token = new JwtSecurityToken(
+                issuer: config["Authentication:Issuer"],
+                audience: config["Authentication:Audience"],
+                claims: claims,
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
         }
 
         public async Task<Response> Register(AppUserDTO appUserDTO)
